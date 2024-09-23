b7dcac6b9b2699042c58d43afe63cb8a2382913f
chriswickens <69883109+chriswickens@users.noreply.github.com>
2024-09-23 16:01:37 -0400

    Updated various names for project files/base class/XAML files.

--- CloneyPadWindow.xaml.cs
+++ CloneyPadWindow.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace CloneyPad
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CloneyPadWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CloneyPadWindow : Window
     {
-        public MainWindow()
+        public CloneyPadWindow()
         {
             InitializeComponent();
         }
